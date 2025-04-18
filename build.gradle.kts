af888c8a27b26b67f958b2f99212d749d35bcfae
Marble <mwszksnmdys@gmail.com>
2025-04-19 00:44:07 +0800

    update IntelliJ plugin version and adjust platform dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 plugins {
     java
     kotlin("jvm") version "1.9.25"
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 java {
@@ -34,7 +34,7 @@ intellijPlatform {
 }
 
 group = "com.baomidou.plugin.idea.ext.mybatisx"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenLocal()
@@ -47,9 +47,8 @@ repositories {
 
 dependencies {
     intellijPlatform{
-        create("IU", "2024.3")
+        create("IU", "2025.1")
         bundledPlugins(listOf("com.intellij.java", "org.jetbrains.kotlin", "com.intellij.database", "com.intellij.spring.boot"))
-        instrumentationTools()
     }
     implementation("com.softwareloop:mybatis-generator-lombok-plugin:1.0")
     implementation("uk.com.robust-it:cloning:1.9.2")
